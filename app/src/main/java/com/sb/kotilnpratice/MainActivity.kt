9bd7ccff04a54f1df5819c12f1d0487ad0e78612
nukana9 <80957343+nukana9@users.noreply.github.com>
2021-05-28 19:28:18 +0530

    I just write a addition of 2 numbers with val and var and set the text to a TextView and button Click fun

--- app/src/main/java/com/sb/kotilnpratice/MainActivity.kt
+++ app/src/main/java/com/sb/kotilnpratice/MainActivity.kt
@@ -2,10 +2,26 @@ package com.sb.kotilnpratice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.sb.kotilnpratice.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view= binding.root
+        setContentView(view)
+        binding.tvHeader.text="Sample Program"
+        binding.tvTitle.text="Addtion of two number"
+
+    }
+
+    fun OnAdition(view: View) {
+        var numberone:Int=Integer.parseInt(binding.etNumberone.text.toString())
+       var  numbertwo:Int=Integer.parseInt(binding.etNumbertwo.text.toString())
+        val sum= numberone + numbertwo
+        binding.tvResult.text="Result:  $sum"
     }
 }
